1d66d2714fd146651aac854999410c98fc4b2a20
SezalVerma <sezalverma.s@gmail.com>
2021-04-06 19:37:56 +0530

    ...

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port = 8000;
 
 const app = express();
 
+// for any url ,use routes from 
+app.use('/' , require('./routes/index'));
+
 app.listen( port , function(err){
     if(err){
         console.log(`Error while firing the server at port : ${port}`);
